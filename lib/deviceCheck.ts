de02c79c9f992972437fd013ea489d1602521b46
Kashew Knutt <kashewknutt@gmail.com>
2025-07-29 16:21:20 +0530

    added few checks

--- lib/deviceCheck.ts
+++ lib/deviceCheck.ts
@@ -2,5 +2,8 @@
 
 export const checkDevice = (): "laptop" | "traditional" => {
   if (typeof window === "undefined") return "traditional"; // Default to traditional
-  return window.innerWidth > 768 ? "laptop" : "traditional";
+  if (window.devicePixelRatio === 1.25) {
+    return window.innerWidth > 1200 ? "laptop" : "traditional";
+  }
+  return "traditional";
 };
